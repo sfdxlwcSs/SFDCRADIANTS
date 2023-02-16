08626bb1b79556b0ecf201c34b4f8b98d2b6d0da
Somnath Sharma <52417989+sfdxlwcSs@users.noreply.github.com>
2023-02-17 00:49:53 +0530

    getetr setter

--- force-app/main/default/lwc/getterSetterChild/getterSetterChild.js
+++ force-app/main/default/lwc/getterSetterChild/getterSetterChild.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+/**
+ * @description       : 
+ * @author            : Somnath Sharma
+ * @group             : 
+ * @last modified on  : 17-02-2023
+ * @last modified by  : Somnath Sharma
+**/
+import { LightningElement, api } from 'lwc';
 
-export default class GetterSetterChild extends LightningElement { }
+export default class GetterSetterChild extends LightningElement {
+    heroData;
+    filteredHeroes;
+
+
+    @api
+    get parentData() {
+        return this.heroData;
+    }
+    //what is this value it is nothing but the data passed from parent
+    set parentData(value) {
+        this.heroData = value;
+        this.filterHeroes();
+    }
+    filterHeroes() {
+        this.filteredHeroes = this.heroData.filter((item) => item.ranking <= 3);
+    }
+
+}
